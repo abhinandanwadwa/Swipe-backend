609f628aad739fb2b60d169fcb1a48aa5429e84d
Abhinandan Wadhwa <74089487+abhinandanwadwa@users.noreply.github.com>
2022-08-18 18:35:09 +0530

    Refactoring db.js

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/swipe";
 
-
 const connectToMongo = () => {
     mongoose.connect(mongoURI);
     console.log("Connected to MongoDB Successfully!!");
 }
-
 
 module.exports = connectToMongo;
